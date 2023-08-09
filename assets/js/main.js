bab7f8ffabfd1dd8603e784756b8d94aef7b2b75
kamran0599 <kamranheyderli049@gmail.com>
2023-08-09 20:38:07 +0400

    add_home_task

---
+++ assets/js/main.js
@@ -0,0 +1,111 @@
+
+let box = document.querySelector(".box");
+let input = document.querySelector("input");
+
+input.addEventListener("input", () => {
+    box.style.borderRadius=input.value;
+    box.style.background=input.value;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const data = [
+//     {
+//         id: 1,
+//         name: "Kamran"
+//     },
+//     {
+//         id: 2,
+//         name: "Hesen"
+//     },
+//     {
+//         id: 3,
+//         name: "Elnur"
+//     },
+//     {
+//         id: 4,
+//         name: "Kmail"
+//     },
+//     {
+//         id: 5,
+//         name: "Sabir"
+//     }
+// ];
+
+// data.map((item) => {
+//     document.querySelector("#user_list").innerHTML += `<li>${item.name}</li>`;
+// })
+
+// const changeColor = (color) => {
+//     document.bgColor = color
+// }
+
+// let number1, number2, Sum;
+// number1 = Number(prompt("ilk reqemi daxil edin"));
+// number2 = Number(prompt("ikinci reqemi daxil edin"));
+// Sum = number1 + number2;
+// console.log("cem reqem:" + Sum);
+
+// let word="Kamran";
+
+// document.write(word)
+
+// var reqem=prompt("Ededinizi daxil edin");
+// if(reqem%2==0 && reqem%5==0){
+//     alert("Sizin reqem 2 ve 5 e tam bolunur")
+// }
+// else{
+//     alert("sizin reqem gecersizdir")
+// }
+
+// var reqem =prompt("reqemi daxil edin")
+// if(reqem<0){
+//     console.log("reqem 0 dan kicikdir")
+// }
+// else if(reqem==0){
+// console.log("reqem 0 aberaberdir")
+// }
+// else{
+//     console.log("reqem 0 dan boyukdur")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
